e5d68bbecf8732dd0286b165d124d5ad63a7f02a
Mohit Patel <mohit.patel1966@gmail.com>
2024-05-24 19:50:33 +0530

    if else for phone message registry

--- src/index.ts
+++ src/index.ts
@@ -24,48 +24,40 @@ botBaileys.on('message', async (message) => {
     console.log(message)
     if(message.body && endsWithWhatsAppNet(message.from)){
       console.info({message}) // keep it here
-      const intervalId = setInterval(() => {
-        console.log("Getting phone registary");
-      }, 1000);
     
-      while (!phoneSettingMapping) {
-          // Waiting for phoneSettingMapping to become present
-      }
-      
-      // Clear the interval once phoneSettingMapping is present
-      clearInterval(intervalId);
-
-      const sender:whatsaphoneSettingMapping = phoneSettingMapping[message.from.slice(2,12)]
-      if (!awaitingResponse && message.type != 'poll') {
-        console.log(sender)
-        if(sender && !awaitingResponse){
-          const pollOptions:string[] = getPollOptions(sender.waAlertSettings);
-          await botBaileys.sendPoll(message.from, `Hello, ${sender.name} Please Select One Of The Options`, {
-            options: pollOptions,
-            multiselect: false
-          })
-        }else{
-          await botBaileys.sendText(message.from, `You Don't Have A Licence To Access Smart Agent Bot. Get Your License From www.smartagent.one Now.`);        
-        }
-        awaitingResponse = true;
-      } else {    
-        try{
-          const resMsg = await processString(message.body,sender.og_id);
-          if(Array.isArray(resMsg)){
-            resMsg.forEach(async(msg:string) => {
-              await botBaileys.sendText(message.from, msg);
-            });
+      if (phoneSettingMapping) {      
+        const sender:whatsaphoneSettingMapping = phoneSettingMapping[message.from.slice(2,12)]
+        if (!awaitingResponse && message.type != 'poll') {
+          if(sender && !awaitingResponse){
+            const pollOptions:string[] = getPollOptions(sender.waAlertSettings);
+            await botBaileys.sendPoll(message.from, `Hello, ${sender.name} Please Select One Of The Options`, {
+              options: pollOptions,
+              multiselect: false
+            })
           }else{
-            await botBaileys.sendText(message.from, resMsg);
+            await botBaileys.sendText(message.from, `You Don't Have A Licence To Access Smart Agent Bot. Get Your License From www.smartagent.one Now.`);        
           }
-          awaitingResponse = false;
-        }catch(e){
-          console.error(e)
-        }     
+          awaitingResponse = true;
+        } else {    
+          try{
+            const resMsg = await processString(message.body,sender.og_id);
+            if(Array.isArray(resMsg)){
+              resMsg.forEach(async(msg:string) => {
+                await botBaileys.sendText(message.from, msg);
+              });
+            }else{
+              await botBaileys.sendText(message.from, resMsg);
+            }
+            awaitingResponse = false;
+          }catch(e){
+            console.error(e)
+          }     
     }}
       else{
-      console.log("body not found in message")
+      phoneSettingMapping = await fetchRequest('/whatsapp/phoneSettingMapping');
+      console.log("phoneSettingMapping not found")
     }
+  }
 });
 
 
